3024322461e4d60b003253817c939a814cc9b157
Danila <68828127+MINeSSTEIN@users.noreply.github.com>
2021-03-23 10:01:04 +0300

    23.03.21 10:00
    *Доработки интерфейса

--- Kursach/Kursach/Res/Pages/NoUser/Login.xaml.cs
+++ Kursach/Kursach/Res/Pages/NoUser/Login.xaml.cs
@@ -27,26 +27,37 @@ namespace Kursach.Res.Pages.NoUser
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            LogMeIn();
+        }
+
+        /// <summary>
+        /// Метод входа пользователя
+        /// </summary>
+        private void LogMeIn()
+        {
             try
             {
-                var usr = Classes.ObjectsVisibility.EntityVision.e.Users.FirstOrDefault(x => x.Telephone == tbLogin.Text && x.Password == pbPassword.Password);
+                var usr = Classes.ObjectsVisibility.EntityVision.e.Users.FirstOrDefault(x => x.Telephone == tbLogin.Text && x.Password == pbPassword.Password); // Создает временный объект пользователя с введенными логином и паролем
 
-                if(usr == null)
+                if (usr == null)
                 {
-                    MessageBox.Show("Такого пользователя нет");
+                    MessageBox.Show("Такого пользователя нет"); // Если данные введены некорректно или не введены вовсе, выводит сообщение о том, что пользователя не существует
                 }
                 else
                 {
-                    switch (usr.Type)
+                    switch (usr.Type) // Если такой пользователь есть, то он проверяет его роль
                     {
-                        case (1):
+                        case (1): // Если это роль пользователя, то загружаются соответствующие страницы
                             Classes.ObjectsVisibility.btnsFrameVision.f.Navigate(new Buttons.Buttons_Guest());
                             Classes.ObjectsVisibility.FrameVision.f.Navigate(new Guest.Main());
 
-                            Classes.ObjectsVisibility.CurrentUserInfo.id = usr.id;
+
+                            // Следующие команды записывают данные о пользователе в соответствующие поля в статическом классе для дальнейшего использования
+                            Classes.ObjectsVisibility.CurrentUserInfo.id = usr.id; 
                             Classes.ObjectsVisibility.CurrentUserInfo.type = usr.Type;
                             break;
-                        default:
+
+                        default: // Если роли пользователя не найдено, то выводится сообщение о том, что ее не существует
                             MessageBox.Show("Такой роли пользователя не существует");
                             break;
                     }
