b30743b1fdf2b6426fdac26d8e4f89b715759692
MINeSSTEIN <68828127+MINeSSTEIN@users.noreply.github.com>
2021-03-23 19:05:12 +0300

    23.03.21
    *Отправил загрузку блюд из базы в отдельный поток
    *Заварил очень вкусный и крепкий чай, конфетка вообще

--- Kursach/Kursach/Res/Pages/NoUser/DishesBrowser.xaml.cs
+++ Kursach/Kursach/Res/Pages/NoUser/DishesBrowser.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Data.Entity;
+using System.Threading;
 
 namespace Kursach.Res.Pages.NoUser
 {
@@ -10,17 +11,29 @@ namespace Kursach.Res.Pages.NoUser
     /// </summary>
     public partial class DishesBrowser : Page
     {
+        DataGrid nota = new DataGrid(); //По приколу создаем "виртуальную" таблицу чтобы пихнуть туда блюда из базы
+        List<InterfaceObjects.DishView> lViews = new List<InterfaceObjects.DishView>(); //Делаем коллекцию с кастомным элементом управления
+
         public DishesBrowser()
         {
             InitializeComponent();
-
-            DataGrid nota = new DataGrid(); //По приколу создаем "виртуальную" таблицу чтобы пихнуть туда блюда из базы
-
             Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Load();//Загружаем таблицу с блюдами из базы данных
             nota.ItemsSource = Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local.ToBindingList(); //Таки пихаем данные из таблицы в таблицу (хвахывхаыва)
-            List<InterfaceObjects.DishView> lViews = new List<InterfaceObjects.DishView>(); //Делаем коллекцию с кастомным элементом управления
-            
 
+            Thread thread1 = new Thread(LoadDishesToViews);
+            thread1.Start();
+            Thread.Sleep(0);
+
+            LoadDishesToViews();
+
+            var l = new Label();
+            l.Content = $"Записей загружено: {lViews.Count}";
+            wpDishes.Children.Add(l);
+        }
+
+
+        private void LoadDishesToViews()
+        {
 
             for (int i = 0; i < nota.Items.Count - 1; i++) //ну в цикле все +/- понятно, но стоит все равно объяснить
             {
@@ -36,9 +49,6 @@ namespace Kursach.Res.Pages.NoUser
                 }
                 catch { }
             }
-            var l = new Label();
-            l.Content = $"Записей загружено: {lViews.Count}";
-            wpDishes.Children.Add(l);
         }
     }
 }
--- Kursach/Kursach/Res/Pages/NoUser/Login.xaml.cs
+++ Kursach/Kursach/Res/Pages/NoUser/Login.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kursach.Res.Pages.NoUser
 {
@@ -41,7 +30,7 @@ namespace Kursach.Res.Pages.NoUser
 
                 if (usr == null)
                 {
-                    MessageBox.Show("Такого пользователя нет"); // Если данные введены некорректно или не введены вовсе, выводит сообщение о том, что пользователя не существует
+                    MessageBox.Show("Такого пользователя нет"); // Если данные введены некорректно или не введены вовсе, выводится сообщение о том, что пользователя не существует
                 }
                 else
                 {
@@ -50,17 +39,15 @@ namespace Kursach.Res.Pages.NoUser
                         case (1): // Если это роль пользователя, то загружаются соответствующие страницы
                             Classes.ObjectsVisibility.btnsFrameVision.f.Navigate(new Buttons.Buttons_Guest());
                             Classes.ObjectsVisibility.FrameVision.f.Navigate(new Guest.Main());
-
-
-                            // Следующие команды записывают данные о пользователе в соответствующие поля в статическом классе для дальнейшего использования
-                            Classes.ObjectsVisibility.CurrentUserInfo.id = usr.id; 
-                            Classes.ObjectsVisibility.CurrentUserInfo.type = usr.Type;
                             break;
 
                         default: // Если роли пользователя не найдено, то выводится сообщение о том, что ее не существует
                             MessageBox.Show("Такой роли пользователя не существует");
                             break;
                     }
+                    // Следующие команды записывают данные о пользователе в соответствующие поля в статическом классе для дальнейшего использования
+                    Classes.ObjectsVisibility.CurrentUserInfo.id = usr.id;
+                    Classes.ObjectsVisibility.CurrentUserInfo.type = usr.Type;
                 }
             }
             catch { }
