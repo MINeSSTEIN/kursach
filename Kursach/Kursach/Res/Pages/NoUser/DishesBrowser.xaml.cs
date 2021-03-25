9f5133d58084ffd47b52edae8e5effc037177d07
MINeSSTEIN <68828127+MINeSSTEIN@users.noreply.github.com>
2021-03-25 20:40:16 +0300

    25.03.21 20:38
    *У каждого блюда теперь отображается картинка
    *Картинки не скругляются
    *Начата работа над страницей регистрации

--- Kursach/Kursach/Res/Pages/NoUser/DishesBrowser.xaml.cs
+++ Kursach/Kursach/Res/Pages/NoUser/DishesBrowser.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Data.Entity;
 using System.Threading;
+using System.Windows.Media.Imaging;
 
 namespace Kursach.Res.Pages.NoUser
 {
@@ -50,16 +51,24 @@ namespace Kursach.Res.Pages.NoUser
         private void LoadDishesToViews()
         {
 
+
             for (int i = 0; i < nota.Items.Count - 1; i++) //ну в цикле все +/- понятно, но стоит все равно объяснить
             {
                 try
                 {
+                    BitmapImage bm = new BitmapImage();
+                    bm.BeginInit();
+                    bm.UriSource = new Uri(System.IO.Directory.GetCurrentDirectory() +
+                        Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local[i].PicturePath);
+                    bm.EndInit();
+
                     lViews.Add(new InterfaceObjects.DishView()); //Добавляем новый элемент в коллекцию
                     decimal t_price = Math.Round(Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local[i].Price, 2); //Округляем циферки в цене
                     lViews[i].lName.Content = Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local[i].Name.ToString(); //Задаем имя...
                     lViews[i].lPrice.Content = $"{t_price} p."; //...цену...
                     lViews[i].lWeight.Content = $"{Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local[i].Weight} г."; //...вес...
-                    lViews[i].lType.Content = Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local[i].Type;
+                    lViews[i].lType.Content = Classes.ObjectsVisibility.EntityVision.e.v_Dishes_With_Types_View.Local[i].Type; //...тип...
+                    lViews[i].iPic.Source = bm; //...картинку
                     wpDishes.Children.Add(lViews[i]);
                 }
                 catch { }
