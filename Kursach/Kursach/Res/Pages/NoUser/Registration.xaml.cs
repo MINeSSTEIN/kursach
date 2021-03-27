e44f107b4d756ec7f60890cce75ff55f227aac15
MINeSSTEIN <68828127+MINeSSTEIN@users.noreply.github.com>
2021-03-27 16:05:04 +0300

    27.03.21 16:04
    *Сделан селектор даты рождения на странице регистрации

--- Kursach/Kursach/Res/Pages/NoUser/Registration.xaml.cs
+++ Kursach/Kursach/Res/Pages/NoUser/Registration.xaml.cs
@@ -21,11 +21,32 @@ namespace Kursach.Res.Pages.NoUser
     public partial class Registration : Page
     {
         private Dictionary<string, int[]> Dates = new Dictionary<string, int[]>();
-        //private List<String>
+        private Dictionary<string, int[]> DatesLoap = new Dictionary<string, int[]>();
+
+        private List<string> Months = new List<string>
+        {
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябр",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь"
+        };
+
         public Registration()
         {
             InitializeComponent();
-            //FillcbS();
+            FillMonths();
+            FillMonthsLoap();
+            FillcbY();
+            FillcbM();
+            FillcbD();
         }
 
         private void FillMonths()
@@ -38,7 +59,7 @@ namespace Kursach.Res.Pages.NoUser
             Dates.Add("Февраль", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+                21, 22, 23, 24, 25, 26, 27, 28});
 
             Dates.Add("Март", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -48,7 +69,7 @@ namespace Kursach.Res.Pages.NoUser
             Dates.Add("Апрель", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
 
             Dates.Add("Май", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -58,7 +79,7 @@ namespace Kursach.Res.Pages.NoUser
             Dates.Add("Июнь", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
 
             Dates.Add("Июль", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -73,7 +94,7 @@ namespace Kursach.Res.Pages.NoUser
             Dates.Add("Сентябрь", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
 
             Dates.Add("Октябрь", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -83,26 +104,122 @@ namespace Kursach.Res.Pages.NoUser
             Dates.Add("Ноябрь", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
 
             Dates.Add("Декабрь", new int[] {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
         }
-        private void FillcbS()
+
+        private void FillMonthsLoap()
         {
-            // Заполнить комбобокс с годом
+            DatesLoap.Add("Январь", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+
+            DatesLoap.Add("Февраль", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29});
+
+            DatesLoap.Add("Март", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+
+            DatesLoap.Add("Апрель", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
+
+            DatesLoap.Add("Май", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+
+            DatesLoap.Add("Июнь", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
+
+            DatesLoap.Add("Июль", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+
+            DatesLoap.Add("Август", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+
+            DatesLoap.Add("Сентябрь", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
+
+            DatesLoap.Add("Октябрь", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+
+            DatesLoap.Add("Ноябрь", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30});
+
+            DatesLoap.Add("Декабрь", new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
+        }
+
+        private void FillcbY()
+        {
             for (int i = DateTime.Now.Year - 18; i > 1900; i--)
             {
                 cbYear.Items.Add(i);
             }
+            cbYear.SelectedIndex = 0;
+        }
 
-            for (int i = 0; i < Dates.Count; i++)
+        private void FillcbM()
+        {
+            for (int i = 0; i < Months.Count; i++)
             {
-                //cbMonth.Items.Add(Dates.);
+                cbMonth.Items.Add(Months[i]);
             }
+            cbMonth.SelectedIndex = 0;
+        }
 
+        private void FillcbD()
+        {
+            if ((int)cbYear.SelectedItem % 4 == 0)
+            {
+                cbDate.ItemsSource = DatesLoap[cbMonth.SelectedItem.ToString()];
+            }
+            else
+            {
+                cbDate.ItemsSource = Dates[cbMonth.SelectedItem.ToString()];
+            }
+            cbDate.SelectedIndex = 0;
+        }
+
+        private void cbMonth_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbMonth.SelectedItem != null)
+            {
+                FillcbD();
+            }
+        }
+
+        private void cbYear_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbMonth.SelectedItem != null)
+            {
+                FillcbD();
+            }
         }
     }
 }
